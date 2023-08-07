2507aad850c16a3dd6e8a49fa1fdb71cfa4762f9
GreenBloodDev <baehr.lukas01@gmail.com>
2023-08-07 23:32:37 +0200

    first style

--- src/index.php
+++ src/index.php
@@ -4,63 +4,69 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog - Start</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<?php
-@include 'util.php';
+<header>
+    <h1>Mein Blog</h1>
+</header>
 
-$per_page = 5;
-$current_page = isset($_GET['page']) ? $_GET['page'] : 1;
-$offset = ($current_page - 1) * $per_page;
+<div class="content">
+    <?php
+    @include 'util.php';
 
-function get_total_blog_posts()
-{
-    $query = mysqli_query(database_blogpost(), "SELECT COUNT(*) as total FROM blog_posts");
-    $data = mysqli_fetch_assoc($query);
-    return $data['total'];
-}
+    $per_page = 5;
+    $current_page = isset($_GET['page']) ? $_GET['page'] : 1;
+    $offset = ($current_page - 1) * $per_page;
 
-$total_pages = ceil(get_total_blog_posts() / $per_page);
-
-$query = mysqli_query(database_blogpost(), "SELECT * FROM blog_posts ORDER BY created_at DESC LIMIT $offset, $per_page");
-
-if ($result = $query) {
-    foreach ($query as $post) {
-        println("Title: " . $post['title']);
-        println("Content: " . $post['content']);
-        println("Created at: " . $post['created_at']);
-        println("");
-        println("");
+    function get_total_blog_posts()
+    {
+        $query = mysqli_query(database_blogpost(), "SELECT COUNT(*) as total FROM blog_posts");
+        $data = mysqli_fetch_assoc($query);
+        return $data['total'];
     }
 
-    if ($total_pages > 1) {
-        echo "<div class='pagination'>";
+    $total_pages = ceil(get_total_blog_posts() / $per_page);
 
-        if ($current_page > 1) {
-            echo "<a href='index.php?page=" . ($current_page - 1) . "'>vorherige </a>";
+    $query = mysqli_query(database_blogpost(), "SELECT * FROM blog_posts ORDER BY created_at DESC LIMIT $offset, $per_page");
+
+    if ($result = $query) {
+        foreach ($query as $post) {
+            echo "<div class='post'>";
+            echo "<h2>" . $post['title'] . "</h2>";
+            echo "<p>" . $post['content'] . "</p>";
+            echo "<p>Erstellt am: " . $post['created_at'] . "</p>";
+            echo "</div>";
         }
 
-        for ($i = 1; $i <= $total_pages; $i++) {
-            if ($i == $current_page) {
-                echo "<span class='current'>$i</span>";
-            } else {
-                echo "<a href='index.php?page=$i'>$i</a>";
+        if ($total_pages > 1) {
+            echo "<div class='pagination'>";
+
+            if ($current_page > 1) {
+                echo "<a href='index.php?page=" . ($current_page - 1) . "'>vorherige </a>";
             }
-            if ($i < $total_pages) {
-                echo ", ";
+
+            for ($i = 1; $i <= $total_pages; $i++) {
+                if ($i == $current_page) {
+                    echo "<span class='current'>$i</span>";
+                } else {
+                    echo "<a href='index.php?page=$i'>$i</a>";
+                }
+                if ($i < $total_pages) {
+                    echo ", ";
+                }
             }
-        }
 
-        if ($current_page < $total_pages) {
-            echo "<a href='index.php?page=" . ($current_page + 1) . "'>, nächste</a>";
-        }
+            if ($current_page < $total_pages) {
+                echo "<a href='index.php?page=" . ($current_page + 1) . "'>, nächste</a>";
+            }
 
-        echo "</div>";
+            echo "</div>";
+        }
     }
-}
-?>
+    ?>
+</div>
 
 </body>
 </html>
