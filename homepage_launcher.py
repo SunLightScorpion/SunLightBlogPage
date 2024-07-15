5055fb991bd278469062494e66c536b8a02a5ac6
NightDev701 <lukasbaehr01@gmail.com>
2024-07-15 10:51:56 +0200

    add run files

---
+++ homepage_launcher.py
@@ -0,0 +1,9 @@
+import os
+import time
+
+while True:
+
+    os.system("clear")
+    time.sleep(1)
+
+    os.system("node homepage.js")
---
+++ run.sh
@@ -0,0 +1,1 @@
+screen -s homepage python3 homepage_launcher.py
